22d6569402b97b7233f96e52e48fc9eba284c0a8
Matthew Lowry <58381202+fritos121@users.noreply.github.com>
2020-06-18 12:42:21 -0400

    Made getting file from FTP, and unzipping .gz file methods use in other programs. Added check for current file before retrieving file.
    Signed-off-by: Fritos121 <58381202+Fritos121@users.noreply.github.com>

--- ftp stuff.py
+++ ftp stuff.py
@@ -1,7 +1,20 @@
 import ftplib
 import os
-import gzip
-import shutil
+
+
+def retrieve_ftp_file(file_name):
+    file = open(file_name, "wb")
+    ftp.retrbinary("RETR " + file_name, file.write)
+    file.close()
+
+
+def unzip_gz(file_name):
+    import gzip
+    import shutil
+    with gzip.open(file_name, 'rb') as f_in:
+        with open(file_name[:-3], 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
 
 # go to ncbi ftp site
 ftp_site = "ftp.ncbi.nlm.nih.gov"
@@ -9,14 +22,14 @@ ftp = ftplib.FTP(ftp_site)
 ftp.login()
 fnbase = "_cds_from_genomic.fna.gz"
 
-# go to current assembly refseq location
+# go to current assembly refseq location (change ftp_pattern in try.py)
 ftp.cwd("genomes/all/GCF/000/005/575/GCF_000005575.2_AgamP3")
-#dirs = ftp.nlst()
+# dirs = ftp.nlst()
 #print(dirs)
 
 # where orgs are being saved
 base_dir = "D:/Orthologs/Codon_Dist/"
-# create dir for specific org using tax_id
+# create dir for specific org using tax_id and make it current working directory
 try:
     os.chdir(base_dir + "7165")
 except OSError:
@@ -26,14 +39,8 @@ except OSError:
 # download archived fna file for all CDS in org
 # make sure file doesn't already exist
 file_name = 'GCF_000005575.2_AgamP3' + fnbase
-file = open(file_name, "wb")
-ftp.retrbinary("RETR " + file_name, file.write)
-file.close()
+if file_name not in os.listdir(os.getcwd()):
+    retrieve_ftp_file(file_name)
+    unzip_gz(file_name)  # unzip CDS fna file
 
-# unzip
-with gzip.open(file_name, 'rb') as f_in:
-    with open(file_name[:-3], 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
-
-# go through each dir and create codon dist dictionaries as txt files, delete uncompressed (both?)
-
+# go through each dir and create codon dist dictionaries as txt files, delete uncompressed
