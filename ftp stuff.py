7a792ff305f0ca8d1c63d82b58079bad1435ebc4
Matthew Lowry <58381202+fritos121@users.noreply.github.com>
2020-06-26 14:53:26 -0400

    made both files importable modules. Other general design changes

--- ftp stuff.py
+++ ftp stuff.py
@@ -2,9 +2,9 @@ import ftplib
 import os
 
 
-def retrieve_ftp_file(local_target_dir, file_name, ftp_obj):
-    file = open(os.path.join(local_target_dir, file_name), "wb")
-    ftp_obj.retrbinary("RETR " + file_name, file.write)
+def retrieve_ftp_file(local_target_dir, destination_file_name, ftp_obj):
+    file = open(os.path.join(local_target_dir, destination_file_name), "wb")
+    ftp_obj.retrbinary("RETR " + destination_file_name, file.write)
     file.close()
 
 
@@ -17,38 +17,43 @@ def unzip_gz(file_name):
             shutil.copyfileobj(f_in, f_out)
 
 
-# go to ncbi ftp site
-ftp_site = "ftp.ncbi.nlm.nih.gov"
-ftp = ftplib.FTP(ftp_site)
-ftp.login()
-fnbase = "_cds_from_genomic.fna.gz"
+if __name__ == "__main__":
+    # go to ncbi ftp site
+    ftp_site = "ftp.ncbi.nlm.nih.gov"
+    ftp = ftplib.FTP(ftp_site)
+    ftp.login()
+    fnbase = "_cds_from_genomic.fna.gz"
 
-# where orgs are being saved
-base_dir = os.path.abspath("D:/Orthologs/Codon_Dist")
+    # where orgs are being saved
+    base_dir = os.path.abspath("D:/Orthologs/Source_Org_Codon_Dist")
 
-# go to current assembly refseq location
-assembly_links = [('/genomes/all/GCF/000/005/575/GCF_000005575.2_AgamP3', '7165'),
-                  ('/genomes/all/GCF/000/209/225/GCF_000209225.1_ASM20922v1', '45351')]
+    # go to current assembly refseq location
+    assembly_links = [('/genomes/all/GCF/000/005/575/GCF_000005575.2_AgamP3', '7165'),
+                      ('/genomes/all/GCF/000/209/225/GCF_000209225.1_ASM20922v1', '45351')]
 
-for assembly, tax_id in assembly_links:
-    # create dir for specific org using tax_id
-    dir_name = os.path.join(base_dir, tax_id)
-    try:
-        os.mkdir(dir_name)
-    except OSError:
-        pass
+    for assembly, tax_id in assembly_links:
+        # create dir for specific org using tax_id
+        dir_name = os.path.join(base_dir, tax_id)
+        try:
+            os.mkdir(dir_name)
+        except OSError:
+            pass
 
-    # download archived fna file for all CDS in org
-    # make sure file doesn't already exist
-    file_name = assembly.split("/")[-1] + fnbase
-    if file_name not in os.listdir(dir_name):
-        ftp.cwd(assembly)  # make it current working directory
-        # dirs = ftp.nlst()
-        # print(dirs)
-        print("Downloading \"" + file_name + "\" from FTP server...")
-        retrieve_ftp_file(dir_name, file_name, ftp)
+        # assembly name always at end of ftp link
+        file_name = assembly.split("/")[-1] + fnbase
 
-        file_name = os.path.join(dir_name, file_name)
-        unzip_gz(file_name)  # unzip CDS fna file
+        # download archived fna file for all CDS in org
+        # make sure file doesn't already exist
+        if file_name not in os.listdir(dir_name):
+            ftp.cwd(assembly)  # make it current working directory
+            # dirs = ftp.nlst()
+            # print(dirs)
+            print("Downloading \"" + file_name + "\" from FTP server...")
+            retrieve_ftp_file(dir_name, file_name, ftp)
 
-# go through each dir and create codon dist dictionaries as txt files, delete uncompressed
+            file_name = os.path.join(dir_name, file_name)
+            unzip_gz(file_name)  # unzip CDS fna file
+
+    # check each tax_id dir to see if more than one assembly was downloaded (4 files present, 6, etc)
+    # go through each dir and create codon dist dictionaries as txt files, delete uncompressed
+    # update local README with date of downloads?
--- try.py
+++ try.py
@@ -1,12 +1,39 @@
 from Bio import Entrez
-from bioservices import UniProt
-import re
 from time import sleep
+import re
 
 
-def get_match(pattern, search_str, index):
+def get_protein_info(uniprot_ids):
+    from bioservices import UniProt
+
+    refseq_vers = []
+    taxonomy_ids = []
+    missing_refseq = []
+    missing_taxid = []
+    u = UniProt()
+
+    uniprot_records = u.retrieve(uniprot_ids, frmt='txt')
+    # print(prot_info[0])
+
+    for i, record in enumerate(uniprot_records):
+        refseq_ver = get_match(refseq_pattern, record, missing_refseq, uniprot_ids, i)
+        taxonomy_id = get_match(taxid_pattern, record, missing_taxid, uniprot_ids, i)
+
+        # make dict to connect ortho uid with refseq ver?
+        refseq_vers.append(refseq_ver)  # refseq version number for coding seq
+        taxonomy_ids.append(taxonomy_id)  # tax_id of organism protein belongs to
+
+    if missing_refseq:
+        print('Proteins Missing RefSeq version numbers: ' + ', '.join(missing_refseq))
+    if missing_taxid:
+        print('Proteins Missing NCBI TaxIDs: ' + ', '.join(missing_taxid))
+
+    return refseq_vers, taxonomy_ids
+
+
+# keep generic function even though on len() == 1 used in main()
+def get_match(pattern, search_str, missing_list, id_list, index):
     match = re.search(pattern, search_str)
-    # do find iter for re?
     try:
         if len(match.groups()) == 1:
             wanted_item = match.group(1)
@@ -14,69 +41,68 @@ def get_match(pattern, search_str, index):
             wanted_item = match.groups()
 
     except AttributeError:
-        if pattern is refseq_pattern:
-            missing_refseq.append(uids[index])
-        elif pattern is taxid_pattern:
-            missing_taxid.append(uids[index])
-        elif pattern is ftp_pattern:
-            missing_ftp.append([uids[index]])
+        missing_list.append(id_list[index])
+        # add placeholder value to keep indexing between refseq and tax_id 1:1?
+        # return None
 
     else:
         print(wanted_item)
         return wanted_item
 
 
+# set email in main()
 def get_assembly_id(tx_id):
 
     search_term = 'txid{}'.format(tx_id)  # search term format used by NCBI Genome db
     handle1 = Entrez.esearch(db='genome', term=search_term) # gets internal ID for genome in genome db
     record1 = Entrez.read(handle1)
-    #print(record1)
+    # print(record1)
     org_id = record1['IdList'][0]    # what if there are more than 1... not encountered yet so don't worry
-    #print(org_id)
+    # print(org_id)
     handle2 = Entrez.esummary(db='genome', id=org_id) # gives assembly of genome org
     record2 = Entrez.read(handle2)
     # print(record2)
     assem_id = record2[0]["AssemblyID"]
-    print(assem_id)
+    # print(assem_id)
 
     # depending on design of program later, make this get entire report for assembly?
 
     return assem_id
 
 
-Entrez.email = 'mlowry2@mymail.vcu.edu'
-refseq_pattern = re.compile(r"DR\s+RefSeq.*(XM_.*).")
-taxid_pattern = re.compile(r"OX\s+NCBI_TaxID=(\d+)")
-ftp_pattern = re.compile(r"<FtpPath_RefSeq>\S*(/genomes\S*)(GCF_\S*)<")
-missing_refseq = []
-missing_taxid = []
-missing_ftp = []
+if __name__ == "__main__":
+    Entrez.email = 'mlowry2@mymail.vcu.edu'
+    refseq_pattern = re.compile(r"DR\s+RefSeq.*(XM_.*).")
+    taxid_pattern = re.compile(r"OX\s+NCBI_TaxID=(\d+)")
+    ftp_pattern = re.compile(r"<FtpPath_RefSeq>\S+(/genomes\S+GCF_\S+)<")
 
-u = UniProt()
-uids = ['A0NAQ1', 'A8DWE3'] # this will be gotten from bioservices.py somehow
-prot_info = u.retrieve(uids, frmt='txt')
+    uids = ['A0NAQ1', 'A8DWE3']  # this will be gotten from bioservices.py (piped from command line)
+    refseq_versions, tax_ids = get_protein_info(uids)
+    # print(refseq_versions, tax_ids)
+    # if implemented, remove placeholders
+    unique_tax_ids = list(set(tax_ids))
 
-# get assembly ID
-ftp_partials = []  # list of partial ftp links to be used later
-for i, record in enumerate(prot_info):
-    # if i == 0:
-    refseq_ver = get_match(refseq_pattern, record, i)
-    tax_id = get_match(taxid_pattern, record, i)
+    # get partial ftp links to be used by ftp module
+    ftp_partials = []  # list of partial ftp links to be used later
+    missing_ftp = []
+    for i, tax_id in enumerate(unique_tax_ids):
+        assembly_id = get_assembly_id(tax_id)
+        handle = Entrez.esummary(db='assembly', id=assembly_id, report='full')
+        string = handle.read()
+        # print(string)
 
-    assembly_id = get_assembly_id(tax_id)
-    handle2 = Entrez.esummary(db='assembly', id=assembly_id, report='full')
-    string = handle2.read()
-    # print(string)
+        # gets ftp (partial) link for all cds in organism
+        ftp_link = get_match(ftp_pattern, string, missing_ftp, unique_tax_ids, i)
+        ftp_partials.append((ftp_link, tax_id))
+        sleep(0.4)  # prevent timeouts from ncbi
 
-    # gets ftp (partial) link for all cds in organism (download into <path>/tax_id/)
-    ftp_matches = get_match(ftp_pattern, string, i)
-    ftp_link = ''.join(ftp_matches)  # + "_cds_from_genomic.fna.gz"
-    # print(ftp_link)
-    ftp_partials.append((ftp_link, tax_id))
-    sleep(0.4)
+    print(ftp_partials)
+    if missing_ftp:
+        print('Proteins Missing FTP Link for Assembly: ' + ', '.join(missing_ftp))
 
-print(ftp_partials)
+    # do something with ortho CDS here. Determine how to store
+    # refseq_versions, uids, and tax_ids should have same length
 
-# ftp_link appended to list of links to be downloaded and unzipped
-# pass match object into function...?
+    # for ortho in enumerate(refseq_versions):
+
+    # pass match object into function...?
