f8184addbc32ce64141416caaae2fb986fc6425c
Fritos121 <58381202+Fritos121@users.noreply.github.com>
2020-08-14 16:04:01 -0400

    added check for name assumption. fixed logic bugs. made getting codon dist function

--- score_msa.py
+++ score_msa.py
@@ -1,13 +1,54 @@
 from Bio import AlignIO
 from Bio.Alphabet import IUPAC, AlphabetEncoder
-from codon_dist import codon_iter
+from codon_dist import codon_iter, flip_trans_table
 import re
-import glob
 import os
+
+def fetch_org_distribution(taxonomy_id, base_dir):
+    """
+    Creates codon distribution for a given organism found in the given directory
+    :param taxonomy_id: string; NCBI tax_id
+    :param base_dir: directory where folders named by tax_id are stored
+    :return: dictionary of codons paired with their frequency in their aa codon distribution
+    """
+    import glob
+    # add empty string to trick join to add one more set of filepath delimiters
+    org_dir = os.path.join(base_dir, taxonomy_id, '')
+    # grabs 1st csv file (should be only one if using whole pipeline)
+    file_list = glob.glob(org_dir + '*_dist.csv')
+    if len(file_list) != 1:
+        exit('Too many or no csv files in directory. Ensure exactly one distribution file exists.')
+
+    dist_fh = open(file_list[0], 'r')
+    codon_dist = {codon: float(freq) for codon, freq in [line.strip().split(',') for line in dist_fh.readlines()]}
+
+    return codon_dist
+
 
 align_in = r'D:\Orthologs\Ortholog_Codon_Dist\PTHR42792\P04949_ortholog_msa_codon3.txt'
 source_org_dir = r'D:\Orthologs\Source_Org_Codon_Dist'
 outdir = r'D:\Orthologs\Ortholog_Codon_Dist\PTHR42792'
+
+tt_11 = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': 'O',
+    'TGC': 'C', 'TGT': 'C', 'TGA': 'U', 'TGG': 'W',
+}
+
+tt_flip = flip_trans_table(tt_11)
 
 msa_alphabet = AlphabetEncoder(IUPAC.ExtendedIUPACProtein(), '-.')
 
@@ -29,12 +70,7 @@ for alignment in alignments:
     header = alignment.id
     # tax_id needed to obtain files containing source org codon dist... used to obtain frequency score for codon
     tax_id = re.search(tax_id_pattern, header).group(1)
-    # add empty string to trick join to add one more set of filepath delimiters
-    org_dir = os.path.join(source_org_dir, tax_id, '')
-    # grabs 1st csv file (should be only one if using whole pipeline)
-    source_dist_fh = open(glob.glob(org_dir + '*.csv')[0], 'r')
-    source_codon_dist = {codon: float(freq) for codon, freq in [line.strip().split(',')
-                                                                for line in source_dist_fh.readlines()]}
+    source_codon_dist = fetch_org_distribution(tax_id, source_org_dir)
 
     alignment_codons = list(codon_iter(str(alignment.seq)))
     freq_scores = ''
@@ -49,14 +85,16 @@ for alignment in alignments:
             freq_scores += 'x'
             continue
 
-        # 1 if codon conserved from subject, 0 if codon was not
-        if codon == subject_codons[i]:
+        # 1 if aa is conserved from subject (synonymous codons), 0 if not
+        aa = tt_11[codon]
+        if aa == tt_11[subject_codons[i]]:
             conserv_scores += '1'
         else:
             conserv_scores += '0'
 
         # 1 for frequent codons, 0 for rare/infrequent (when comparing the otho's source org codon dist to uniform dist)
-        if source_codon_dist[codon] >= 0.5:
+        codon_count = len(tt_flip[aa])      # counts number of codons per aa to get uniform dist below
+        if source_codon_dist[codon] >= (1/codon_count):
             freq_scores += '1'
         else:
             freq_scores += '0'
