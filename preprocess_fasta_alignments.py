050abde3126261ea0ac915975b987c8ecaed6b45
Fritos121 <58381202+Fritos121@users.noreply.github.com>
2020-11-12 22:39:34 -0500

    added command line interface; comments for clarity

--- preprocess_fasta_alignments.py
+++ preprocess_fasta_alignments.py
@@ -1,8 +1,16 @@
 from Bio import AlignIO, SeqIO
 from Bio.Alphabet import IUPAC, AlphabetEncoder
 from codon_dist import codon_iter
+import sys
+
 
 def try_translate(codon, translation_table):
+    """
+    Translate a codon with error handling
+    :param codon: string; 3 DNA nucleotides (ACGT)
+    :param translation_table: dict; translation table of 'codon':'amino-acid' pairs
+    :return: Either a translated amino acid, or None
+    """
     try:
         aa = translation_table[codon]
     except KeyError:
@@ -13,13 +21,24 @@ def try_translate(codon, translation_table):
 
 if __name__ == '__main__':
 
-    align_in = r'D:\Orthologs\Ortholog_Codon_Dist\PTHR43553\P33941_ortholog_msa.txt'
-    cds_in = r'D:\Orthologs\Ortholog_Codon_Dist\PTHR43553\P33941_ortholog_cds.fna'
-    error_in = r'D:\Orthologs\Ortholog_Codon_Dist\PTHR43553\P33941_removed_uids.txt'
-    format = 'fasta'
+    if len(sys.argv) != 4:
+        exit(f"Required positional arguments: {sys.argv[0]} <alignment file> <Fasta cds file> <error file>")
+
+    align_in = sys.argv[1]
+    cds_in = sys.argv[2]
+    error_in = sys.argv[3]
+
+    # get list of uids that had errors; used to prevent assert errors for previously considered errors
+    error_fh = open(error_in, 'r')
+    # same uid can be in file many times; reduce search time in lower loop
+    error_uids = set([line.strip() for line in error_fh.readlines()])
+    error_fh.close()
 
     align_out = ''.join(align_in.split('.')[:-1]) + '_codon.txt'
+    file_format = 'fasta'
 
+    # allow for selenocysteine (TGA=U) (https://en.wikipedia.org/wiki/Selenocysteine)
+    # allow for pyrrolysine (TAG=O) (https://en.wikipedia.org/wiki/Pyrrolysine)
     tt_11 = {
         'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
         'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
@@ -39,57 +58,46 @@ if __name__ == '__main__':
         'TGC': 'C', 'TGT': 'C', 'TGA': 'U', 'TGG': 'W',
     }
 
-    # get list of uids that had errors; used to prevent assert errors
-    error_fh = open(error_in, 'r')
-    error_uids = [line.strip() for line in error_fh.readlines()]
-    error_fh.close()
-
-    # check if alphabet required in long run... later
     # adds gap both gap characters seen in PANTHER msa
     msa_alphabet = AlphabetEncoder(IUPAC.ExtendedIUPACProtein(), '-.')
 
     # the cds and alignment files should be in the same order except for uids in error file, allowing for easy indexing
-    coding_seqs = list(SeqIO.parse(cds_in, format, alphabet=IUPAC.IUPACUnambiguousDNA()))
+    coding_seqs = list(SeqIO.parse(cds_in, file_format, alphabet=IUPAC.IUPACUnambiguousDNA()))
 
     codon_alignments = []
     found_flag = False
     i = 0
-    for alignment in AlignIO.read(align_in, format, alphabet=msa_alphabet):
-        # if uid is one that errored previously, then it was removed from the cds file
+    for alignment in AlignIO.read(align_in, file_format, alphabet=msa_alphabet):
+        # if uid is one that error'ed previously, then it was removed from the cds file
         if alignment.id in error_uids:
-            continue  # don't increment i if error found; should realign files
+            continue  # don't increment i if error found; realigns files
 
         # coding seq header needs split, can be made more general for all fasta later...
         cds_header = coding_seqs[i].id
-        cds_uid = cds_header.split(';')[0][4:]
-        # any uids not in error list that don't match corresponding entry in cds file crash out
-        assert (alignment.id == cds_uid), "Alignment ID: " + alignment.id + " doesn't match Coding Sequence ID: " + cds_uid
+        cds_uid = cds_header.split(';')[0][4:]  # header formatted from get_species_info.py
 
-        #print(len(alignment.seq))
-        #print(len(str(coding_seqs[i].seq)))
+        # any uids not in error list that don't match corresponding entry in cds file crash out
+        assert (alignment.id == cds_uid), f"Alignment ID: {alignment.id} doesn't match Coding Sequence ID: {cds_uid}"
 
         # don't need Seq object, just make string to make iterator more clear
         codon_seq = list(codon_iter(str(coding_seqs[i].seq)))
-        #print(len(codon_seq))
-        #print(codon_seq)
         codon_position = 0      # marks where we are in the list of codons
         aligned_coding_seq = ''     # translated from aa to DNA
         for aa in alignment.seq:
-
             if aa == '.' or aa == '-':
                 aligned_coding_seq += aa * 3
+                # don't increment since no amino acid validated; codon from codon_seq no used
 
             else:
-                #print(codon_position)
-                # fix later; indicates an alignment seq that is longer than the coding seq
+                # indicates an alignment seq that is longer than the coding seq
                 try:
                     codon = codon_seq[codon_position]
                 except IndexError:
-                    print(alignment.id, cds_header)
-                    aligned_coding_seq += 'QQQ'
-                    continue      # counts how many AA longer align seq is than codon_iter
-                    # break
-                #print(codon, codon_position * 3)
+                    print(f"\n{alignment.id} Alignment Seq longer than corresponding Coding Seq {cds_header}")
+                    aligned_coding_seq += 'QQQ'   # add as marker for removal during MSA cleaning
+                    break
+                    # continue      # counts how many AA longer align seq is than codon_iter, keeps seq in MSA
+
                 # make sure codon isn't an open frame (at end of seq)
                 if len(codon) % 3 == 0:
                     # check if aa in aligned seq translates to corresponding codon in codon_seq
@@ -104,9 +112,8 @@ if __name__ == '__main__':
                     # for when mismatch occurs; get alignment id, and alignment and cds positions
                     else:
                         aligned_coding_seq += 'NNN'
-                        # print('oops')
 
-                codon_position += 1
+                codon_position += 1  # only increment when non-gap char found
 
         # make gene of interest that will have all genes aligned to it for analysis first
         if alignment.id in align_in:
